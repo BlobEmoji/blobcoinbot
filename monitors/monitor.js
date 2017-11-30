61667ecfa8242e3579bc0433ce57c8dae78d5905
OGNova <jamooney2000@gmail.com>
2017-11-30 18:16:34 +0000

    Add files via upload

--- monitors/monitor.js
+++ monitors/monitor.js
@@ -12,7 +12,7 @@ module.exports = class {
   }
   
   async run(client, message, level) {
-    this.givePoints(client, message, level);
+    this.dropPoints(client, message, level);
   }
 
   static givePoints(client, message, level) {
@@ -42,20 +42,20 @@ module.exports = class {
 
   async dropPoints(client, message, level) {
     if (message.channel.type !== 'text') return;
-    setInterval(() => {
-      const actions = ['mine', 'drill', 'blob', 'coin'];
-      const score = client.points.get(`${message.guild.id}-${message.author.id}`) || { points: 1, level: 0, user: message.author.id, guild: message.guild.id };
-      const settings = client.settings.get(message.guild.id);
-      const pickMethod = `${actions[Math.floor(Math.random() * actions.length)]}`
-      const response = await this.client.awaitReply(message, `Respond with ${settings.prefix}${pickMethod} to get a random amount of Blob Coins!`);
-      if ([`${pickMethod}`].includes(response.toLowerCase())) {
-        console.log('Blob Coin mined!');
-        await response.delete();
-        const points = (parseInt(settings.chatDrop));
-        score.points += points;
-        message.channel.send(`${message.author.username} grabbed the coins!`);
-      }
-    }, parseInt(settings.chatDropRate));
+    console.log('Shit\'s happening');
+    const actions = ['mine', 'drill', 'blob', 'coin'];
+    const score = client.points.get(`${message.guild.id}-${message.author.id}`) || { points: 1, level: 0, user: message.author.id, guild: message.guild.id };
+    const settings = client.settings.get(message.guild.id);
+    const pickMethod = `${actions[Math.floor(Math.random() * actions.length)]}`;
+    const response = await this.client.awaitReply(message, `Respond with ${settings.prefix}${pickMethod} to get a random amount of Blob Coins!`);
+    if ([`${pickMethod}`].includes(response.toLowerCase())) {
+      console.log('Blob Coin mined!');
+      await response.delete();
+      const points = (parseInt(settings.chatDrop));
+      score.points += points;
+      message.channel.send(`${message.author.username} grabbed the coins!`);
+    }
+    setInterval(dropPoints, settings.chatDropRate);
   }
 
 };
